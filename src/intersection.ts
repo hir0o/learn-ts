d7365905e28544e79b685fe91111e7470478b809
shibuyahiroyuki <mk.9.q.kyu@gmail.com>
2020-11-21 12:06:34 +0900

    finish intersection

---
+++ src/intersection.ts
@@ -0,0 +1,30 @@
+export {};
+
+type Pitcher1 = {
+  throwingSpeed: number;
+};
+
+type Batter1 = {
+  battingAverage: number;
+};
+
+const DaimajinSasaki: Pitcher1 = {
+  throwingSpeed: 154,
+};
+
+const OchiaiHiromitsu: Batter1 = {
+  battingAverage: 0.367,
+};
+
+// type TowWayPlayer = {
+//   throwingSpeed: number;
+//   battingAverage: number;
+// };
+
+/// Pitcher1 と Batter1 の型を併せ持つ型
+type TowWayPlayer = Pitcher1 & Batter1;
+
+const OtaniShohei: TowWayPlayer = {
+  throwingSpeed: 165,
+  battingAverage: 0.285,
+};
