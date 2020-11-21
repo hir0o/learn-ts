72ef5ec63c4e4a735649532a298da11d0f67826c
shibuyahiroyuki <mk.9.q.kyu@gmail.com>
2020-11-21 11:59:55 +0900

    finish unknown

---
+++ src/unknown.ts
@@ -0,0 +1,15 @@
+export {};
+// any型より，ちょっと型安全？
+
+const kasoku = (): number => 43;
+
+// なんの値が入るかわからない．
+let numberAny: any = kasoku();
+let numberUnknown: unknown = kasoku();
+
+let samAny = numberAny + 10;
+// if文で囲んで，型を確認するとエラーにならない．
+// typeガードっていう
+if (typeof numberUnknown === "number") {
+  let samUnknown = numberUnknown + 10;
+}
